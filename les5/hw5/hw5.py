5f80ce009f4ba79931a32e1d11b9bc5d7c083f6c
Михаил Пискунов <piskunov.m.a@gmail.com>
2019-12-10 13:57:27 +0300

    les5 all hw

--- les5/hw5/hw5.py
+++ les5/hw5/hw5.py
@@ -4,4 +4,16 @@
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
 
-my_f = open("text5.txt", "a")
+my_f = open("text5.txt", "w")
+numbers = input('Введите строку чисел, разделенных пробелом: \n')
+try:
+    my_f.write(numbers)
+    my_f.close()
+
+    my_f = open("text5.txt", "r")
+    data_file = my_f.read()
+    result = sum(map(int, data_file.split()))
+    print(result)
+    my_f.close()
+except ValueError:
+    print("Введите числа, а не символы!")
